1fc83893c71819a3b075327eaa80fa9c7b8f3bc8
Claire Bear <clairebear20080508@gmail.com>
2024-09-20 10:20:36 +0800

    c

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/mosquito-egg-counting-software' : '/',
   outputDir: 'docs'  // 使用 'docs' 而不是默認的 'dist'
 }
